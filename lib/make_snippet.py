0ddcad52a3fd091e2c953feaf587fb04f2a58dd7
Abbas <iamabbasalam@gmail.com>
2023-10-05 10:45:25 +0100

    golden square challenges

---
+++ lib/make_snippet.py
@@ -0,0 +1,10 @@
+def make_snippet(text):
+
+    words_list = text.split(' ')
+    if len(words_list) > 5:
+        first_five = words_list[:5]
+        snippet = ' '.join(first_five)
+        return snippet + '...'
+    else:
+        return text
+
